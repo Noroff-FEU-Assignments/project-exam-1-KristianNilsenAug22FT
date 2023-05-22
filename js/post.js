493b166f2c6fa03f8d5197e0d683f7f08301e0e1
KristianNilsenAug22FT <114563561+KristianNilsenAug22FT@users.noreply.github.com>
2023-05-22 23:15:22 +0200

    commit

--- js/post.js
+++ js/post.js
@@ -1,5 +1,7 @@
 const postId = new URLSearchParams(window.location.search).get('id');
 const postContent = document.getElementById('postContent');
+const modal = document.getElementById('myModal');
+const modalImage = document.getElementById('modalImage');
 
 async function fetchPost() {
   try {
@@ -12,11 +14,38 @@ async function fetchPost() {
 }
 
 function renderPost(post) {
-  document.title = post.title.rendered;
-  postContent.innerHTML = `
-    <h2>${post.title.rendered}</h2>
-    <div>${post.content.rendered}</div>
-  `;
+    document.title = post.title.rendered;
+    postContent.innerHTML = `
+      <h2>${post.title.rendered}</h2>
+      <div>${post.content.rendered}</div>
+    `;
+  
+    // Extract the image URL from the API response
+    const imageUrlMatch = post.content.rendered.match(/src="([^"]+)"/);
+    if (imageUrlMatch && imageUrlMatch.length > 1) {
+      const imageUrl = imageUrlMatch[1];
+  
+      // Add event listener to the post image
+      const postImage = postContent.querySelector('img');
+      postImage.addEventListener('click', () => openModal(imageUrl));
+    }
+  }
+  
+
+function openModal(imageUrl) {
+  modal.style.display = 'block';
+  modalImage.src = imageUrl;
 }
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close the modal when the user clicks outside the image
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
 
 fetchPost();
